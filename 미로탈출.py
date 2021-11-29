9dcf5d2142a7cfe507b074d3c600993ed30f33e3
hong1468 <48309403+hong1468@users.noreply.github.com>
2021-11-29 22:20:11 +0900

    BFS/미로탈출

---
+++ 미로탈출.py
@@ -0,0 +1,44 @@
+from collections import deque
+
+n, m = map(int, input().split())
+
+graph = []
+
+for i in range(n):
+    graph.append(list(map(int, input())))
+
+# 상, 하, 좌, 우
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+
+# BFS 소스코드 구현
+def bfs(x, y):
+    # 큐(Queue) 구현을 위해 deque 라이브러리 사용
+    queue = deque()
+    queue.append((x, y))
+
+    # queue가 빌 때까지 반복
+    while queue:
+        x, y = queue.popleft()
+
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            # 범위 검사
+            if nx < 0 or ny < 0 or nx >= n or ny >= m:
+                continue
+
+            # 벽 검사
+            if graph[nx][ny] == 0:
+                continue
+
+            # 처음 방문시 최단 거리 기록
+            if graph[nx][ny] == 1:
+                graph[nx][ny] = graph[x][y] + 1
+                queue.append((nx, ny))
+    return graph[n - 1][m - 1]
+
+
+print(bfs(0, 0))
